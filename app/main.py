b587f75bad1fc0901443b81e1a149f0dac385a10
bh-premnath-git <Premnath@bighammer.ai>
2025-07-18 13:47:11 +0530

    changes in dockerize

--- app/main.py
+++ app/main.py
@@ -228,4 +228,4 @@ async def value_error_handler(request, exc):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
